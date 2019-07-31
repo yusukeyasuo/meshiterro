109698f084ff39007e31685cdb20942decf9473f
Yusuke Yasuo <yasuo.y@dely.jp>
2019-07-31 21:06:04 +0000

    feat: post_images

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '572b5e7b6aec7714881ef12de8af9e036399f9af8a66921590396948ce291c192c3ce816711be04c7ed34ac22be5c28559af53cc4a528eb16aebce364bcbbeb0'
+
